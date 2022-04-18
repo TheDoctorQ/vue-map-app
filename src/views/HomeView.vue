438b5b549b63fae75f9c18ec2da02edda5856e99
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-04-18 13:46:41 -0600

    creates map app, hides api key

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
+<script>
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+    };
+  },
+  mounted: function () {
+    this.makeMap();
+  },
+  methods: {
+    makeMap: function () {
+      mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_KEY;
+      const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-73.9859723, 40.6701033], // starting position [lng, lat]
+        zoom: 12 // starting zoom
+      });
+    }
+  },
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <button v-on:click="makeMap()">Make Map</button>
+    <div id='map' style='width: 800px; height: 600px;'></div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style>
+</style>
